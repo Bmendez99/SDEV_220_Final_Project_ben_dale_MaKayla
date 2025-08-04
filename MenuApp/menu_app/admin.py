3250ed1644170e2dbd50e51591534e6d28cca09c
ben <bmendez15@ivytech.edu>
2025-08-03 20:31:19 -0400

    added image

--- MenuApp/menu_app/admin.py
+++ MenuApp/menu_app/admin.py
@@ -2,9 +2,22 @@ from django.contrib import admin
 from .models import Item
 
 class MenuItemAdmin(admin.ModelAdmin):
-    list_display = ("meal", "status")
-    list_filter = ("status",)
+    list_display = ("meal", "price", "meal_type", "status", "date_created")
+    list_filter = ("status", "meal_type", "date_created")
     search_fields = ("meal", "description")
-
+    readonly_fields = ("date_created", "date_updated")
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('meal', 'description', 'price', 'meal_type', 'status')
+        }),
+        ('Image', {
+            'fields': ('image',),
+            'classes': ('collapse',)
+        }),
+        ('Metadata', {
+            'fields': ('author', 'date_created', 'date_updated'),
+            'classes': ('collapse',)
+        }),
+    )
 
 admin.site.register(Item, MenuItemAdmin)
